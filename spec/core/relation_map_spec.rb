ae3cd69c1381ecf056d0118928fc0abb8cd6503f
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-08-22 12:53:31 +0200

    Amend RelationMap spec to reflect new behaviour

--- spec/core/relation_map_spec.rb
+++ spec/core/relation_map_spec.rb
@@ -10,21 +10,24 @@ describe Halibut::Core::RelationMap do
   end
 
   describe '#add' do
-    it "has a single item per relation" do
-      subject.add 'first' , { value: 'first' }
-      subject.add 'second', { value: 'second' }
-
-      assert_equal 'first', subject['first'].first[:value]
-      assert_equal 'second', subject['second'].last[:value]
+    context "when there was no prior relation" do
+      it "has one relation" do
+        subject.add 'first', { value: 'first'}
+        assert_equal 'first', subject['first'].first[:value]
+      end
     end
 
-    it "has various items per relation" do
-      subject.add 'first', { value: 'first' }
-      subject.add 'first', { value: 'second' }
+    context "when there was prior relation" do
+      context "and the relation was a single object" do
+        it "has an array of two relations" do
+          subject.add 'first', { value: 'first'}
+          subject.add 'first', { value: 'second'}
 
-      subject['first'].length.must_equal  2
-      subject['first'].first[:value].must_equal 'first'
-      subject['first'].last[:value].must_equal  'second'
+          subject['first'].length.must_equal 2
+          subject['first'].first[:value].must_equal 'first'
+          subject['first'].last[:value].must_equal  'second'
+        end
+      end
     end
 
     # todo: throw an exception if add receives a value that does not respond to to_hash
@@ -36,33 +39,19 @@ describe Halibut::Core::RelationMap do
   end
 
   describe '#to_hash' do
-    describe 'when single item arrays become objects' do
+    describe 'single item and multi item relations' do
       it 'generates single item relations correctly' do
         subject.add('person', { name: 'bob' })
 
         subject.to_hash['person'][:name].must_equal 'bob'
       end
 
-      it 'generates multi item relations correctly' do
-        subject.add('person', { name: 'bob' })
-        subject.add('person', { name: 'floyd' })
+      it 'handles single item array relations' do
+        value = [{ name: 'bob' }]
+        subject.add('person', [{ name: 'bob' }])
 
-        hashed_people = subject.to_hash['person']
-        hashed_people.length.must_equal 2
-        hashed_people[0][:name].must_equal 'bob'
-        hashed_people[1][:name].must_equal 'floyd'
-      end
-    end
-
-    describe 'when single item arrays stay arrays' do
-      subject { Halibut::Core::RelationMap.new(single_item_arrays: true) }
-
-      it 'generates single item relations correctly' do
-        subject.add('person', { name: 'bob' })
-
-        hashed_people = subject.to_hash['person']
-        hashed_people.length.must_equal 1
-        hashed_people[0][:name].must_equal 'bob'
+        hashed = subject.to_hash
+        hashed['person'].must_equal value
       end
 
       it 'generates multi item relations correctly' do
