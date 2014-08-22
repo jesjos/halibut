c581446642886f01d20727793fc062ab7baade5e
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-08-22 12:47:29 +0200

    Handle single item relations in RelationMap in a new way. Try, awkwardly, to combine correctness (preserve single item vs array embeds) and ease of use (always return arrays when accessing relations). Do this by adding +set+ method and checking prior relations, and by wrapping accessed relations in arrays. Granted, this may lead to some confusion, as embedded['relation'] always returns an array, but the resulting hash may contain a single object instead. But, for most users, this should not be a problem.

--- lib/halibut/core/relation_map.rb
+++ lib/halibut/core/relation_map.rb
@@ -6,16 +6,20 @@ module Halibut::Core
   class RelationMap
     extend Forwardable
 
-    def_delegators :@relations, :[], :empty?, :==, :fetch
-
-    DEFAULT_OPTIONS = { single_item_arrays: false }
+    def_delegators :@relations, :empty?, :==
 
     def initialize(options = {})
       @relations = {}
-      @options = DEFAULT_OPTIONS.merge(options)
     end
 
     # Adds an object to a relation.
+    # Keeps track of single object relations for the purpose of serialization to json.
+    # But for ease of use, relations will be returned as arrays.
+    #
+    # If the relation doesn't exist, the relation will point to the object.
+    # If the relation exists and is an array, the object is added to the array.
+    # If the relation exists and is a single object,
+    # the existing object and the incoming object are wrapped in an array.
     #
     # @example
     #     relations = RelationMap.new
@@ -25,41 +29,83 @@ module Halibut::Core
     #          @options=#<Halibut::Core::Link::Options:0x007fa0ca5b9240
     #          @templated=nil, @type=nil, @name=nil, @profile=nil,
     #          @title=nil, @hreflang=nil>>]
+    #     relations.to_hash
+    #     # => { 'self' => { 'href' => '/resource/1'} }
     #
     # @param [String] relation relation that the object belongs to
     # @param [Object] item     the object to add to the relation
     def add(relation, item)
-      unless item.respond_to?(:to_hash)
-        raise ArgumentError.new('only items that can be converted to hashes with #to_hash are permitted')
+      validate_item!(item)
+      if @relations.has_key?(relation)
+        add_to_existing(relation, item)
+      else
+        set(relation, item)
       end
+    end
 
-      @relations[relation] = @relations.fetch(relation, []) << item
+
+    # Sets an object to a relation. Overwrites previous relation.
+    #
+    # @param [String] relation  relation that the object belongs to
+    # @param [Object] item      the object to set to the relation
+    def set(relation, item)
+      validate_item!(item)
+      @relations[relation] = item
+    end
+
+
+    # Always returns an array
+    def [](relation)
+      Halibut::Utilities.array_wrap(@relations[relation])
+    end
+
+
+    # Always returns an array
+    def fetch(*args, &block)
+      Halibut::Utilities.array_wrap(@relations.fetch(*args, &block))
     end
 
     # Returns a hash corresponding to the object.
     #
-    # RelationMap doens't just return @relations because it needs to convert
+    # RelationMap doesn't just return @relations because it needs to convert
     # correctly when a relation only has a single item.
     #
     # @return [Hash] relation map in hash format
     def to_hash
       @relations.each_with_object({}) do |(rel,val), obj|
         rel = rel.to_s
-
-        hashed_val = val.map(&:to_hash)
-        if val.length == 1 && !single_item_arrays?
-          hashed_val = val.first.to_hash
+        if val.is_a?(Array)
+          hashed_val = val.map(&:to_hash)
+        else
+          hashed_val = val.to_hash
         end
-
         obj[rel] = hashed_val
       end
     end
 
-    # Returns true if the relation map is configured to always to
-    # permit single arrays when to_hash is called. The default behavior
-    # is to convert single item arrays into instances
-    def single_item_arrays?
-      @options[:single_item_arrays]
+  private
+
+    def validate_item!(item)
+      valid = if item.is_a?(Array)
+                item.all? {|sub_item| sub_item.respond_to?(:to_hash) }
+              else
+                item.respond_to?(:to_hash)
+              end
+      unless valid
+        raise ArgumentError, 'only items that can be converted to hashes with #to_hash are permitted'
+      end
     end
+
+    def add_to_existing(relation, item)
+      current = @relations[relation]
+      if current.respond_to?(:<<)
+        Halibut::Utilities.array_wrap(item).map do |subitem|
+          @relations[relation] << subitem
+        end
+      else
+        @relations[relation] = [current, item]
+      end
+    end
+
   end
 end
